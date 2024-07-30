86556f98d13833062eac11410054ede7860315de
Xavier <xavieruxchamp@gmail.com>
2024-07-29 18:03:00 -0700

    chore: add raw data to stage table

--- create_database_and_tables.sql
+++ create_database_and_tables.sql
@@ -1,30 +1,56 @@
-﻿BEGIN
-    DECLARE @DatabaseName NVARCHAR(255) = 'KoreAssignment_' + QUOTENAME('Xavier_Toledo');
-    EXEC('CREATE DATABASE ' + @DatabaseName);
-END;
-GO
+﻿IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'KoreAssignment_Xavier_Toledo')
 
-USE @DatabaseName
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'stg')
 BEGIN
-    EXEC('CREATE SCHEMA stg');
+	CREATE DATABASE [KoreAssignment_Xavier_Toledo];
 END
 GO
 
--- ... (similar checks and creation for 'prod' schema and tables)
-
-CREATE TABLE stg.Users (
-    StgID INT IDENTITY(1,1) PRIMARY KEY,
-    UserID INT,
-    FullName NVARCHAR(255),
-    Age INT,
-    Email NVARCHAR(255),
-    RegistrationDate DATE,
-    LastLoginDate DATE,
-    PurchaseTotal DECIMAL(19, 4) -- Using DECIMAL
-);
+USE [KoreAssignment_Xavier_Toledo];
 GO
 
--- ... (prod.Users table with DECIMAL)
+-- Check and create stg schema if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'stg')
+BEGIN
+	EXEC('CREATE SCHEMA stg');
+END
+GO
+
+-- Check and create prod schema if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'prod')
+BEGIN
+	EXEC('CREATE SCHEMA prod');
+END
+GO
+
+-- Check and create stg.Users table if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'stg.Users') AND type in (N'U'))
+BEGIN
+	CREATE TABLE stg.Users (
+		StgID INT IDENTITY(1,1) PRIMARY KEY,
+		UserID INT,
+		FullName NVARCHAR(255),
+		Age INT,
+		Email NVARCHAR(255),
+		RegistrationDate DATE,
+		LastLoginDate DATE,
+		PurchaseTotal FLOAT
+	);
+END
+GO
+
+-- Check and create prod.Users table if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'prod.Users') AND type in (N'U'))
+BEGIN
+	CREATE TABLE prod.Users (
+		ID INT IDENTITY(1,1) PRIMARY KEY,
+		UserID INT,
+		FullName NVARCHAR(255),
+		Age INT,
+		Email NVARCHAR(255),
+		RegistrationDate DATE,
+		LastLoginDate DATE,
+		PurchaseTotal FLOAT,
+		RecordLastUpdated DATETIME DEFAULT GETDATE()
+	);
+END
+GO
